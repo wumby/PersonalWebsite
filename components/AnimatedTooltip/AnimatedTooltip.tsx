9fe56b433e2826cc1ca3db3dabaaf1decb13e049
Jack Ziegler <64660004+wumby@users.noreply.github.com>
2025-01-29 12:41:54 -0700

    fix logos on cards

--- components/AnimatedTooltip/AnimatedTooltip.tsx
+++ components/AnimatedTooltip/AnimatedTooltip.tsx
@@ -81,7 +81,7 @@ export const AnimatedTooltip = ({
             width={20}
             src={item.image}
             alt={item.name}
-            className="object-cover !m-0 !p-0 object-top rounded-full h-10 w-10 border-2 group-hover:scale-105 group-hover:z-30 border-white  relative transition duration-500"
+            className="bg-black-100 object-cover !m-0 !p-0 object-top rounded-full h-10 w-10 border-2 group-hover:scale-105 group-hover:z-30 border-white  relative transition duration-500"
           />
         </div>
       ))}
